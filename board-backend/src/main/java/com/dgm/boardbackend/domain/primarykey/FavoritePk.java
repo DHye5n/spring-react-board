e64c78b3ae2a089d9e9aac57176b8677e56fa4c0
dhy25n <sai4875@naver.com>
2024-05-16 23:58:32 +0900

    3th

--- board-backend/src/main/java/com/dgm/boardbackend/domain/primarykey/FavoritePk.java
+++ board-backend/src/main/java/com/dgm/boardbackend/domain/primarykey/FavoritePk.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class FavoritePk extends Serializable {
+public class FavoritePk implements Serializable {
 
     @Column(name = "user_email")
     private String userEmail;
